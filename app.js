73e46d4cd54d6912d89ef9e86ef83411317d8924
Kritika-2 <kritikaagrahari42@gmail.com>
2024-11-30 22:42:14 +0530

    Event listener is added for the button

---
+++ app.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    let h3=document.querySelector("h3");
+    let randomColor= getRandomColor();
+    h3.innerText= randomColor;
