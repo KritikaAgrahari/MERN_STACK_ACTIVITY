ed5c6f245a0f37de9e3c000b6bba7b03ca09b3d8
Kritika-2 <kritikaagrahari42@gmail.com>
2024-11-30 22:43:09 +0530

    a random number for color is generated between range 0 to 255 using Math.random function for rgb

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ btn.addEventListener("click",function(){
     let h3=document.querySelector("h3");
     let randomColor= getRandomColor();
     h3.innerText= randomColor;
+    let div=document.querySelector("div");
+    div.style.backgroundColor=randomColor;
